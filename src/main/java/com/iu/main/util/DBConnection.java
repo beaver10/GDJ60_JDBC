1da0ff9d11ac31c09b6906c2d6d483d2909ab079
hayoung jang <wkdgkdud1005@gmail.com>
2023-01-17 17:36:13 +0900

    test

---
+++ src/main/java/com/iu/main/util/DBConnection.java
@@ -0,0 +1,38 @@
+package com.iu.main.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBConnection {
+	
+	public void getConnection() throws Exception {
+		//1. ID
+		String username="hr";
+		//2. PW
+		String password="hr";
+		//3. url
+		String url="jdbc:oracle:thin:@localhost:1521/XEPDB1";
+		//4. driver
+		String driver="oracle.jdbc.driver.OracleDriver";
+		
+		Class.forName(driver);
+		
+		Connection connection = DriverManager.getConnection(url, username, password);
+		
+		String sql="SELECT * FROM LOCATIONS WHERE LOCATION_ID=1";
+		
+		PreparedStatement st = connection.prepareStatement(sql);
+		
+		ResultSet rs =st.executeQuery();
+		
+		while(rs.next()) {
+			System.out.println(rs.getInt("LOCATION_ID"));
+//			System.out.println(rs.getString("POSTAL_CODE"));
+//			System.out.println(rs.getString("STREET_ADDRESS"));
+		}
+		
+	}
+
+}
